aedee9f5499993bafd35eb3da73cccb800acb49b
AldairAlburqueque <aldairalburqueque8@gmail.com>
2025-12-11 15:22:08 -0500

    Actualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import BooksPage from './pages/BookPage';
 import LoansPage from './pages/LoanPage';
 
 
-const App: React.FC = () => {
+function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Navigate to="/books" />} />
+
         <Route path="/books" element={<BooksPage />} />
         <Route path="/loans" element={<LoansPage />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
